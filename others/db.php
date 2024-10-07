7c89ccc17b2596ef812dcfd175fc89d15b235050
Saymo-Michael <entercapslock72@gmail.com>
2024-10-07 10:28:11 +0800

    Test

--- others/db.php
+++ others/db.php
@@ -5,6 +5,7 @@ $username = "root";
 $password = "";
 $database = "taskmanager";
 
+
 try {
     $connection = new mysqli($host, $username, $password, $database);
 } catch (mysqli_sql_exception $e) {
