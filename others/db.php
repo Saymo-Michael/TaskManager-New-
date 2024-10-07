2c2ef6d1052db34e9a84df8c11e9978aa2b09161
Saymo-Michael <entercapslock72@gmail.com>
2024-10-07 11:13:43 +0800

    Update db.php

--- others/db.php
+++ others/db.php
@@ -5,6 +5,8 @@ $username = "root";
 $password = "";
 $database = "taskmanager";
 
+
+
 try {
     $connection = new mysqli($host, $username, $password, $database);
 } catch (mysqli_sql_exception $e) {
